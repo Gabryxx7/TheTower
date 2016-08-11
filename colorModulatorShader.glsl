98e3470e637b76d2b3b6dfab6789b5b7e47a05e0
Beriol <beriol@hotmail.it>
2016-08-11 18:26:51 +0200

    Added support for the Vive to Level1
    - Now the levels can be completed with the controllers; it's very hard
    to debug without them though and there are A LOT of cases to consider,
    so there might still be bugs here and there
    - Added an option in Level1 to keep every drawing (i.e. the drawings
    won't disappear automatically); to switch from keep/unkeep press Q in
    the keyboard or the menu button in the Vive controllers

--- colorModulatorShader.glsl
+++ colorModulatorShader.glsl
@@ -19,6 +19,8 @@ void main (void)
 {	
 	vec3 color;
 	
+	// I mix the color based on the time variable, but only if it didn't reach 1.0; otherwise I simply use the final color,  
+	// since it would be the result wanted anyway (otherwise with the time variable going above 0 the mix would end up wrong)
 	if(time <= 1.0)
 		color = mix(startingColor, finalColor, time);
 	else
