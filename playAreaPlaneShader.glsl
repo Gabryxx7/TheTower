df307288257e9550572c46d7ca31845297813e33
Beriol <beriol@hotmail.it>
2016-09-26 17:52:04 +0200

    Switched the play area plane with a box

--- playAreaPlaneShader.glsl
+++ playAreaPlaneShader.glsl
@@ -1,12 +1,10 @@
 #version 400
 
-/* NOTE: in order to render the box nicely with this shader, the box has to rendered first culling the front faces (so that only back faces will be drawn) */
-/* and then culling the back faces. By doing this, the back faces will be drawn first and their fragment won't be discarded and the transparencies of the fragments */
-/* will be computed correctly */
+/* The code of this shader is similar to the code in the playAreaBosShader.glsl, so most of the comments are there */
 
 [VERTEX SHADER]
 
-uniform mat4 modelMatrix;
+uniform mat4 modelMatrix;           // Model matrix of the object; used to obtain the world position of the vertices
 
 varying vec4 position;
 varying vec4 worldPosition;
@@ -25,30 +23,41 @@ void main()
 uniform vec3 primaryColor;          // Color of the plane
 uniform vec3 secondaryColor;        // Color of the plane when the player is too close to the edge
 uniform float width;    			// Width of the box
+uniform float height; 
 uniform float depth;   				// Depth of the box
 uniform float outerEdgesTickness;   // How thick are the edges of the box
 uniform vec3 playerPosition;        // Position of the player in the play area
 uniform float colorAlpha;
+uniform float scaleFactor;
 
 varying vec4 position;              // Interpolated position of the vertex, in object space
 varying vec4 worldPosition;         // Interpolated position of the vertex, in world space
 
-uniform mat4 modelMatrix;
-
 void main() 
 {		
+	float currentHeight = position.y + height / 2.0;
+
 	// Now I check if the fragment is in one of the edges. Taking into account that the origin of the object's coordinate system is in the center
 	// of the box, I consider the absolute values of the z and x coordinates to check if the fragment is at the edge, with respect of the given thickness
-	if(abs(position.x) > width / 2.0 -  outerEdgesTickness || abs(position.y) > depth / 2.0 -  outerEdgesTickness)
+	if(abs(position.x) > width / 2.0 -  outerEdgesTickness || abs(position.z) > depth / 2.0 -  outerEdgesTickness)
 	{
-		playerPosition.z = -playerPosition.z;
+	//	playerPosition.z = -playerPosition.z;
+		playerPosition.y -= height / 2.0;
 	
-		if(distance(worldPosition.xyz, playerPosition) > 1.5)
+		// If we're far away from this fragment, I draw it with the primary color (green); red otherwise. The value 1.5 is basically 1.5 meters
+		if(distance(worldPosition.xyz, playerPosition) > 1.2 && currentHeight <= 0.01 * scaleFactor)
 			gl_FragColor = vec4(primaryColor, colorAlpha);
-		else
-			gl_FragColor = vec4(secondaryColor, colorAlpha);
+//		else 
+//			discard;
+	 	else if(distance(worldPosition.xyz, playerPosition) <= 1.2)
+	 		gl_FragColor = vec4(secondaryColor, colorAlpha);
+ 		else
+ 			discard;
 	}	
 	// Otherwise, if the fragment is nearly in the floor, I draw it but with a little alpha value
-	else
+	else if(currentHeight < outerEdgesTickness)
 		gl_FragColor = vec4(primaryColor, 0.2);
+	// Any other fragments (i.e., the top of the box) will be discarded
+	else
+		discard;
 }
