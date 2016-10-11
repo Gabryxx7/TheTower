8cc8dddf45affd747eefb6744cdb2cbeb17ecb15
Gabriele Marini <giaga7@gmail.com>
2016-10-11 14:00:00 +0200

    Modified enemies and added some borders

--- SimpleColorShader.glsl
+++ SimpleColorShader.glsl
@@ -6,7 +6,16 @@ void main(void){
 
 [FRAGMENT SHADER]
 uniform vec3 diffuse;
+uniform vec3 outlinecolor;
+uniform bool outline;
 
 void main (void){
-	gl_FragColor = vec4(diffuse, 1.0);
+	vec4 finalColor;
+	if(outline == 1){
+		finalColor = vec4(diffuse, 1.0);
+	}else{
+		finalColor = vec4(outlinecolor, 1.0);
+	}
+	
+		gl_FragColor = finalColor;
 }
